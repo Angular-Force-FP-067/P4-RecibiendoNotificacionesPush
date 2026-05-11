dfd9b33369d62df2612869f60a2ed94f81f44040
sergiogomezgu <sgomezguti@uoc.edu>
2026-05-11 20:19:05 +0200

    Registro de firebase messaging service worker.

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -3,4 +3,11 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/firebase-messaging-sw.js')
+        .catch((err) => console.error('Service worker error:', err));
+    }
+  })
   .catch((err) => console.error(err));
